8af22f838295e84d530674b1647cc235e1e9fb95
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-03-03 15:45:23 +0200

    update(scripts): use overheadWork functon to add scripts instead of add inline scripts in /docs

--- src/components/views/Docs/index.ts
+++ src/components/views/Docs/index.ts
@@ -9,6 +9,22 @@ export default class extends DummyView {
     this.setTitle();
   }
 
+  async overheadWork(): Promise<void> {
+    document.getElementById("docs")!.onscrollend = (e) => {
+      const docsEle = e.currentTarget as HTMLDivElement;
+      const gapValue = 3; // px
+      const btt = document.getElementById("docs-btt")!;
+      const h = Math.ceil(docsEle.getBoundingClientRect().height) + gapValue;
+      const scrollTop = Math.ceil(docsEle.scrollTop);
+      const show = scrollTop > h + gapValue;
+      btt.style.visibility = `${show ? "visible" : "hidden"}`;
+    };
+
+    document.getElementById("docs-btt")!.onclick = () => {
+      document.getElementById("docs")!.scrollTo({ top: 0, behavior: "smooth" });
+    };
+  }
+
   async getHTML() {
     return template;
   }
